7472d0fbe8501f57a8a65f68a116c05d436b525b
Technomann <marci.reiter501@gmail.com>
2022-10-23 15:34:17 +0200

    khf4 progress

--- khf4/khf4.pl
+++ khf4/khf4.pl
@@ -1,22 +1,46 @@
-satrak_mx(RowNum-ColNum, [HeadTreeRow-HeadTreeCol|TailTrees], [HeadDir|TailDirs], boolMx) :- 
+satrak_mx(RowNum-ColNum, Trees, Dirs, boolMx) :- 
+    [] = CurrentTents,
+    [] = Rows,
+    create_every_tent(RowNum, ColNum, Trees, Dirs, CurrentTents),
+    create_matrice(RowNum, ColNum, CurrentTents, Rows).    
+
+create_matrice(RowNum, ColNum, CurrentTents, Rows) :-
+    build_rows(RowNum, ColNum, 0, CurrentTents, Rows).
+
+build_rows(MaxRow, MaxCol, CurrentRow, CurrentTents, Rows) :-
+    build_cols(MaxCol, CurrentRow, 0, CurrentTents, Row),
+    build_rows(MaxRow, MaxCol, CurrentRow + 1, CurrentTents, [Row|Rows]).
+
+build_rows(MaxRow, _, CurrentRow, _, _) :-
+    CurrentRow == MaxRow, !.
+
+build_cols(MaxCol, CurrentRow, CurrentCol, CurrentTents, Row) :-
+    (memberchk(CurrentRow-CurrentCol, CurrentTents) , 
+        build_cols(MaxCol, CurrentRow, CurrentCol + 1, CurrentTents, ['1'|Row]));
+    (\+memberchk(CurrentRow-CurrentCol, CurrentTents) , 
+        build_cols(MaxCol, CurrentRow, CurrentCol + 1, CurrentTents, ['0'|Row])).
+    
+build_cols(MaxCol, CurrentCol, _, _, _) :-
+    CurrentCol == MaxCol, !.
+
+create_every_tent(RowNum, ColNum, [HeadTree|TailTrees], [HeadDir|TailDirs], CurrentTents) :-
+    placeable_tent(RowNum, ColNum, HeadTree, HeadDir, CurrentTents),
+    create_every_tent(RowNum, ColNum, TailTrees, TailDirs, CurrentTents).
+
+create_every_tent(_, _, [], [], CurrentTents) :-
+    CurrentTents == CurrentTents.
 
 placeable_tent(RowNum, ColNum, TreeRow-TreeCol, Dir, CurrentTents) :- 
-    dir_inside(RowNum, ColNum, create_tent(TreeRow, TreeCol, Dir)),
-    no_collision(create_tent(TreeRow, TreeCol, Dir, Tent), CurrentTents).
+    create_tent(TreeRow, TreeCol, Dir, Tent),
+    tent_inside(RowNum, ColNum, Tent),
+    \+memberchk(Tent, CurrentTents).
 
 create_tent(TreeRow, TreeCol, Dir, Tent) :-
-    (Dir == n , NewRow = TreeRow - 1 , NewRow-TreeCol is Tent);
-    (Dir == s , NewRow = TreeRow + 1 , NewRow-TreeCol is Tent);
-    (Dir == w , NewCol = TreeCol - 1 , TreeRow-NewCol is Tent);
-    (Dir == e , NewCol = TreeCol + 1 , TreeRow-NewCol is Tent).
+    (Dir == n , NewRow is TreeRow - 1 , NewRow-TreeCol = Tent);
+    (Dir == s , NewRow is TreeRow + 1 , NewRow-TreeCol = Tent);
+    (Dir == w , NewCol is TreeCol - 1 , TreeRow-NewCol = Tent);
+    (Dir == e , NewCol is TreeCol + 1 , TreeRow-NewCol = Tent).
 
-dir_inside(RowNum, ColNum, TentRow-TentCol) :-
-    (TentRow > 0 , TentRow <= RowNum , TentCol > 0 , TentCol <= ColNum).
-
-no_collision(TentRow-TentCol, [HeadCurrentTentRow-HeadCurrentTentCol|TailCurrentTents]) :-
-    not(TentRow = HeadCurrentTentRow),
-    no_collision(TentRow-TentCol, TailCurrentTents).
-
-no_collision(TentRow-TentCol, []) :-
-    1 == 1.
+tent_inside(RowNum, ColNum, TentRow-TentCol) :-
+    (TentRow > 0 , TentRow =< RowNum , TentCol > 0 , TentCol =< ColNum).
  
