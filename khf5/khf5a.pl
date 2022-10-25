0506acf280773aa5ef97e78ecb21db9068507f92
Technomann <marci.reiter501@gmail.com>
2022-10-25 23:45:40 +0200

    part1

---
+++ khf5/khf5a.pl
@@ -0,0 +1,38 @@
+iranylistak(RowCol, Trees, DirectionList) :-
+    create_dir_lists(RowCol, Trees, DirectionList).
+
+iranylistak(_, [], DirectionList) :-
+    DirectionList = [].
+
+create_dir_lists(RowCol, Trees, DirectionList) :-
+    [HeadTree|TailTree] = Trees,
+    check_dirs_for_tree(RowCol, [n,s,w,e], HeadTree, Trees,  ListForTree),
+    sort(ListForTree, Sorted),
+    DirectionList = [Sorted|NewDirList],
+    create_dir_lists(RowCol, TailTree, NewDirList).
+
+create_dir_lists(_, [], NewDirList) :-
+    NewDirList = [].
+
+check_dirs_for_tree(RowNum-ColNum, [AllDirHead|AllDirTail], Tree, Trees, CurrentDirs) :-
+    (create_tent(Tree, AllDirHead, Tent),
+    tent_inside(RowNum, ColNum, Tent),
+    \+memberchk(Tent, Trees) ->  
+    	CurrentDirs = [AllDirHead|NewCurrentDirs],
+        check_dirs_for_tree(RowNum-ColNum, AllDirTail, Tree, Trees, NewCurrentDirs)
+    ;   
+    	CurrentDirs = [_|NewCurrentDirs],
+    	check_dirs_for_tree(RowNum-ColNum, AllDirTail, Tree, Trees, NewCurrentDirs)
+    ).
+
+check_dirs_for_tree(_, [], _, _, NewCurrentDirs) :-
+    NewCurrentDirs = [].
+
+create_tent(TreeRow-TreeCol, Dir, Tent) :-
+    (Dir == n , NewRow is TreeRow - 1 , NewRow-TreeCol = Tent);
+    (Dir == s , NewRow is TreeRow + 1 , NewRow-TreeCol = Tent);
+    (Dir == w , NewCol is TreeCol - 1 , TreeRow-NewCol = Tent);
+    (Dir == e , NewCol is TreeCol + 1 , TreeRow-NewCol = Tent).
+
+tent_inside(RowNum, ColNum, TentRow-TentCol) :-
+    (TentRow > 0 , TentRow =< RowNum , TentCol > 0 , TentCol =< ColNum).
