f55014d991b53fe02dd0da951a469a52ca522258
Technomann <marci.reiter501@gmail.com>
2022-10-26 23:36:52 +0200

    Almost done

--- khf5/khf5b.pl
+++ khf5/khf5b.pl
@@ -6,16 +6,44 @@ sator_szukites(Trees, SpecialTreeIndex, TreesDirLists, ShrunkLists) :-
     [Dir|_] = DirsForIndexedTree,
     create_tent(Tree, Dir, Tent),
     ShrunkLists = NewList,
-    magic(Tent, TreesDirLists, NewList).
+    magic(Tent, Tree, Trees, TreesDirLists, NewList).
 
 sator_szukites(_, _, [], ShrunkList) :-
     ShrunkList = [].
 
-magic(Tent, TreesDirLists, ShrunkLists) :-
-    ShrunkLists = [Tent|Coords],
-    collect_coords(Tent, Coords, 0).
-    /*ShrunkLists = ReturnedLists,
-    shrunk_lists(Coords, TreesDirLists, ReturnedLists).*/
+magic(Tent, Tree, Trees, TreesDirLists, ShrunkLists) :-
+    collect_coords(Tent, Coords, 0),
+    CheckingCoords = [Tent|Coords],
+    ShrunkLists = ReturnedLists,
+    shrunk_lists(CheckingCoords, Tree, Trees, TreesDirLists, ReturnedLists).
+
+shrunk_lists(CheckingCoords, SelectedTree, [HeadTree|TailTrees], [HeadTreeDirsList|TailTreeDirsLists], ReturnedLists) :-
+    (SelectedTree == HeadTree -> 
+        ReturnedLists = [HeadTreeDirsList|NewReturnedLists],
+        shrunk_lists(CheckingCoords, SelectedTree, TailTrees, TailTreeDirsLists, NewReturnedLists)
+    ;
+        ReturnedLists = [Sorted|NewReturnedLists],
+        new_dirs_tree(CheckingCoords, HeadTree, HeadTreeDirsList, NewDirList),
+        sort(NewDirList, Sorted),
+        shrunk_lists(CheckingCoords, SelectedTree, TailTrees, TailTreeDirsLists, NewReturnedLists)
+    ).
+
+shrunk_lists(_, _, [], [], ReturnedLists) :-
+    ReturnedLists = [].
+
+new_dirs_tree(CheckingCoords, Tree, [HeadDir|TailDirs], DirList) :-
+    create_tent(Tree, HeadDir, Tent),
+    (memberchk(Tent, CheckingCoords) ->
+        DirList = NewDirList,
+        new_dirs_tree(CheckingCoords, Tree, TailDirs, NewDirList)
+    ;
+        DirList = [HeadDir|NewDirList],
+        new_dirs_tree(CheckingCoords, Tree, TailDirs, NewDirList)
+    ).
+
+new_dirs_tree(_, _, [], NewDirList) :-
+    NewDirList = [].
+
 
 collect_coords(Tent, Coords, Index) :-
     coord_by_index(Tent, Index, Coord),
